1a3788fe35f57c4ab3621348616a90196399be07
Halwahm <90171683+Halwahm@users.noreply.github.com>
2023-12-07 08:59:33 +0300

    add labs

---
+++ 3 COURSE/СТР web-приложений/Lab_3/3b/lab3b/StaticData/StaticData.cs
@@ -0,0 +1,11 @@
+﻿using lab7.Models;
+
+namespace lab7.StaticData;
+
+public static class StaticData
+{
+    public static List<User> users = new()
+    {
+        new User() { Email = "admin@belstu.by", Password = "halwa2003", Roles = new[] { Roles.Administrator } },
+    };
+}
