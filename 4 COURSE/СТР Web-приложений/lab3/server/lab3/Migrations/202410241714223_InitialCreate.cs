38a91f4c219e83ff274795522a3f0aa66c1ecc30
Halwahm <90171683+Halwahm@users.noreply.github.com>
2024-10-24 20:35:58 +0300

    облака 6. бизнес 2. смелик 3

---
+++ 4 COURSE/СТР Web-приложений/lab3/server/lab3/Migrations/202410241714223_InitialCreate.cs
@@ -0,0 +1,27 @@
+﻿namespace lab3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Phone = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Students");
+        }
+    }
+}
